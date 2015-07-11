83b686b0b6e5d6d47ec438e16409c31be79a3222
catxii <catxii@qq.com>
2015-07-11 17:57:54 +0800

    Revert "Revert 543d2c8..da31ee3"
    This reverts commit 53e0d9f273072030ecb6b14bb5907e41ce17391c.

--- js/controllers.js
+++ js/controllers.js
@@ -1,15 +1,19 @@
+
 // 商城
-iapp.controller('ProductCtrl', ['$scope',function($scope) {
-	$scope.products = [
-		{'imgsrc':'images/cp-img.jpg','num':'12','dw':'包'},
-		{'imgsrc':'images/cp-img.jpg','num':'3','dw':'包'},
-		{'imgsrc':'images/cp-img.jpg','num':'12','dw':'包'},
-		{'imgsrc':'images/cp-img.jpg','num':'2','dw':'包'},
-		{'imgsrc':'images/cp-img.jpg','num':'12','dw':'包'},
-		{'imgsrc':'images/cp-img.jpg','num':'12','dw':'包'},
-		{'imgsrc':'images/cp-img.jpg','num':'12','dw':'包'},
-		{'imgsrc':'images/cp-img.jpg','num':'12','dw':'包'},
-	]
+iapp.controller('ProductCtrl', ['$scope','$http',function($scope, $http) {
+	
+	$http({
+		url:"http://testwuhan.ybgplatform.com/product/ajaxBrandList",
+		method:"GET",
+		dataType: "json",
+		cache:true
+	}).success(function(data){
+	    //console.log(data[i])  
+	    $scope.products = data;
+	    console.log("123"+products[0])
+	}).error(function(data){
+		alert("请求失败")
+	})
 }]);
 
 // 个人中心 
@@ -22,4 +26,31 @@ iapp.controller('CartCtrl', ['$scope',function($scope) {
 
 // 订单
 iapp.controller('OrderCtrl', ['$scope',function($scope) {
+}]);
+
+// 用户登陆控制器
+iapp.controller('LoginCtrl', ['$scope', 'APIService', '$state',function($scope, APIService, $state) {
+	
+	// 登陆信息
+	$scope.user = {};
+	
+	// 登陆
+	$scope.signIn = function() {
+		
+		var user = $scope.user;
+		
+		// 判断用户名密码是否为空
+		if (!user.username || !user.password) {
+			// toast 
+			alert('用户名密码不能为空！');
+			return;
+		}
+		// 登陆
+		APIService.login(user,function(data) {
+			alert('登陆成功');
+			$state.go('tabs.mall', {});
+		}, function() {
+			
+		});
+	}
 }]);
