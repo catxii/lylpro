d4269b44b9224121a91e66e5833ef97ef138fd1b
Liang Yun Long <liangyunlong@gmail.com>
2015-07-12 12:35:51 +0800

    Revert "Revert "Revert 2500c74..83b686b""
    This reverts commit 6586131971c8c19a9b82666a6cfd2f39c4258540.

--- js/controllers.js
+++ js/controllers.js
@@ -14,6 +14,32 @@ iapp.controller('ProductCtrl', ['$scope','$http',function($scope, $http) {
 	}).error(function(data){
 		alert("请求失败")
 	})
+
+	// $.ajax({
+ //        url:"http://testwuhan.ybgplatform.com/product/ajaxBrandList",
+ //        type:'GET',
+ //        dataType: "json",
+ //        success:function (data) {
+ //            for(var i=0;i < data.length;i++){
+	//             //console.log(data[i])  
+	//             $scope.products = [];
+	//             $scope.products.push(data[i])
+	//             console.log("123"+products[0])
+ //        	}
+ //        }        
+ 
+ //    });
+
+	// $scope.products = [
+	// 	{'imgsrc':'images/cp-img.jpg','name':'示范产品1','num':'12','dw':'包'},
+	// 	{'imgsrc':'images/cp-img.jpg','name':'示范产品2','num':'3','dw':'包'},
+	// 	{'imgsrc':'images/cp-img.jpg','name':'示范产品3','num':'12','dw':'包'},
+	// 	{'imgsrc':'images/cp-img.jpg','name':'示范产品4','num':'2','dw':'包'},
+	// 	{'imgsrc':'images/cp-img.jpg','name':'示范产品5','num':'12','dw':'包'},
+	// 	{'imgsrc':'images/cp-img.jpg','name':'示范产品6','num':'12','dw':'包'},
+	// 	{'imgsrc':'images/cp-img.jpg','name':'示范产品7','num':'12','dw':'包'},
+	// 	{'imgsrc':'images/cp-img.jpg','name':'示范产品8','num':'12','dw':'包'},
+	// ]
 }]);
 
 // 个人中心 
@@ -26,31 +52,4 @@ iapp.controller('CartCtrl', ['$scope',function($scope) {
 
 // 订单
 iapp.controller('OrderCtrl', ['$scope',function($scope) {
-}]);
-
-// 用户登陆控制器
-iapp.controller('LoginCtrl', ['$scope', 'APIService', '$state',function($scope, APIService, $state) {
-	
-	// 登陆信息
-	$scope.user = {};
-	
-	// 登陆
-	$scope.signIn = function() {
-		
-		var user = $scope.user;
-		
-		// 判断用户名密码是否为空
-		if (!user.username || !user.password) {
-			// toast 
-			alert('用户名密码不能为空！');
-			return;
-		}
-		// 登陆
-		APIService.login(user,function(data) {
-			alert('登陆成功');
-			$state.go('tabs.mall', {});
-		}, function() {
-			
-		});
-	}
 }]);
